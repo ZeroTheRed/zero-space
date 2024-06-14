aba4b4412154d8d075ec2a37c5080895bba8c97c
Tushar Sairam <tushar421337@gmail.com>
2024-06-14 19:54:15 +0530

    Quartz sync: Jun 14, 2024, 7:54 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -43,7 +43,7 @@ const config: QuartzConfig = {
           dark: "#EFC11A",
           secondary: "#E7741E",
           tertiary: "#665993",
-          highlight: "#0F4AC6",
+          highlight: "rgba(31, 29, 69, 0.20)",
         },
       },
     },
